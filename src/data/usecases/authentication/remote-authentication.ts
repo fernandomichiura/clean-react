b517e8b006cf058b3d0c28cead38f56d2688c7c5
Fernando Michiura <fernandomichiura@gmail.com>
2021-08-02 22:31:53 -0300

    chore: changing importing format

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,7 +1,7 @@
 import { HttpPostClient, HttpStatusCode } from "@/data/protocols/http";
 import { Authentication, AuthenticationParams } from "@/domain/usecases";
 import { InvalidCredentialsError, UnexpectedError } from "@/domain/errors";
-import { AccountModel } from "@/domain/models/account-model";
+import { AccountModel } from "@/domain/models/";
 
 export class RemoteAuthentication implements Authentication {
   constructor(
