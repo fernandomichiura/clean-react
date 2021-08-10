950f605de5035aaa718d7a604ce3f13843aec233
Fernando Michiura <fernandomichiura@gmail.com>
2021-08-09 22:09:15 -0300

    test: ensure SignUp show valid status for password field if validation succeeds

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -79,4 +79,10 @@ describe('SignUp Component', () => {
     Helper.populateField(sut, 'email')
     Helper.testStatusForField(sut, 'email')
   })
+
+  test('Should show valid password state if validation succeeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'password')
+    Helper.testStatusForField(sut, 'password')
+  })
 })
