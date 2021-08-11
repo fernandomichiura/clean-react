58a5ecb40d9f4d76cd6c7e60a51b8202dac7de66
Fernando Michiura <fernandomichiura@gmail.com>
2021-08-10 21:15:42 -0300

    feat: add SignUp Composition

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom'
 import { Router } from '@/presentation/components'
 import '@/presentation/styles/global.scss'
 import { makeLogin } from './factories/pages/login/login-factory'
+import { makeSignUp } from './factories/pages/signup/signup-factory'
 
 ReactDOM.render(
   <Router
-    makeLogin={makeLogin}/>,
+    makeLogin={makeLogin}
+    makeSignUp={makeSignUp}
+  />,
   document.getElementById('main')
 )
