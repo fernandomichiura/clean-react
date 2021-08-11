1d77cc3c0c0d3ddee3cdb8cbb8f92be4db4c72d1
Fernando Michiura <fernandomichiura@gmail.com>
2021-08-10 21:05:05 -0300

    refactor: change Validation to receive an object instead of value

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,28 +1,36 @@
 import { RequiredFieldValidation, EmailValidation, MinLengthValidation } from '@/validation/validators'
 import { ValidationBuilder as sut } from './validation-builder'
+import { CompareFieldsValidation } from '../compare-fields/compare-fields-validation'
 import faker from 'faker'
 
 describe('ValidationBuilder', () => {
-  test('Shpuld return RequiredFieldValidation', () => {
+  test('Should return RequiredFieldValidation', () => {
     const field = faker.database.column()
     const validations = sut.field(field).required().build()
     expect(validations).toEqual([new RequiredFieldValidation(field)])
   })
 
-  test('Shpuld return EmailValidation', () => {
+  test('Should return EmailValidation', () => {
     const field = faker.database.column()
     const validations = sut.field(field).email().build()
     expect(validations).toEqual([new EmailValidation(field)])
   })
 
-  test('Shpuld return MinLengthValidation', () => {
+  test('Should return MinLengthValidation', () => {
     const field = faker.database.column()
     const length = faker.datatype.number()
     const validations = sut.field(field).min(length).build()
     expect(validations).toEqual([new MinLengthValidation(field, length)])
   })
 
-  test('Shpuld return a list of validations', () => {
+  test('Should return CompareFieldsValidation', () => {
+    const field = faker.database.column()
+    const fieldToCompare = faker.database.column()
+    const validations = sut.field(field).sameAs(fieldToCompare).build()
+    expect(validations).toEqual([new CompareFieldsValidation(field, fieldToCompare)])
+  })
+
+  test('Should return a list of validations', () => {
     const field = faker.database.column()
     const length = faker.datatype.number()
     const validations = sut.field(field).required().min(length).email().build()
