1b6fdcf0f020eb847eb3da3ef3531ee5a0595fbf
Fernando Michiura <fernandomichiura@gmail.com>
2021-08-12 21:39:52 -0300

    chore: setting dynamic field to testInputStatus

--- src/main/test/cypress/support/form-helper.ts
+++ src/main/test/cypress/support/form-helper.ts
@@ -3,7 +3,7 @@ const baseUrl: string = Cypress.config().baseUrl
 export const testInputStatus = (field: string, error?: string): void => {
   cy.getByTestId(`${field}-wrap`).should('have.attr', 'data-status', error ? 'invalid' : 'valid')
   const attr = `${error ? '' : 'not.'}have.attr`
-  cy.getByTestId('email').should(attr, 'title', error)
+  cy.getByTestId(field).should(attr, 'title', error)
   cy.getByTestId(`${field}-label`).should(attr, 'title', error)
 }
 
