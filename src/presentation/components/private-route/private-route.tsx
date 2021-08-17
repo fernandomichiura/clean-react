a5a27adf2fd674f64bc1c780246a744e26889bc7
Fernando Michiura <fernandomichiura@gmail.com>
2021-08-16 23:22:18 -0300

    feat: ensure PrivateRoute redirects to login if empty token is provided

---
+++ src/presentation/components/private-route/private-route.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Redirect, Route, RouteProps } from 'react-router-dom'
+
+
+const PrivateRoute: React.FC<RouteProps> = (props: RouteProps) => {
+  return <Route {...props} component={() => <Redirect to='/login' />} />
+}
+
+export default PrivateRoute
