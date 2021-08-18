8e73cda2560ec24d1859c9fd28f6a73295d8ad4c
Fernando Michiura <fernandomichiura@gmail.com>
2021-08-17 21:43:18 -0300

    refactor: move survey list items to component

---
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.tsx
@@ -0,0 +1,22 @@
+import Styles from './survey-item-styles.scss'
+import { Icon, IconName } from '@/presentation/components'
+import React from 'react'
+
+const SurveyItem: React.FC = () => {
+  return (
+    <li className={Styles.surveyItemWrap}>
+      <div className={Styles.surveyContent}>
+        <Icon className={Styles.iconWrap} iconName={IconName.thumbUp} />
+        <time>
+          <span className={Styles.day}>13</span>
+          <span className={Styles.month}>08</span>
+          <span className={Styles.year}>2021</span>
+        </time>
+        <p>Qual é seu framework web favorito?</p>
+      </div>
+      <footer>Ver Resultado</footer>
+    </li>
+  )
+}
+
+export default SurveyItem
